ca76b98361ab05f51d94057a27fd6d53448fa223
shev4enk0 <33554718+shev4enk0@users.noreply.github.com>
2017-11-13 00:29:23 +0200

    9_PB_CU2 Physics and Rigidbodies 42. Physics and Rigidbodies

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        ProsesInput();
+    }
+
+    void ProsesInput()
+    {   
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+            print("A Pressed");
+        else if (Input.GetKey(KeyCode.D))
+            print("D Pressed");
+        
+    }
+}
