9175ad3db349465fe6f7e0cb2ee090f88d3e6383
shev4enk0 <33554718+shev4enk0@users.noreply.github.com>
2017-11-16 01:07:46 +0200

    localMovementTry

---
+++ Assets/localMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class localMove : MonoBehaviour
+{
+    public Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.InverseTransformDirection(direction);
+       
+    }
+}
