d1b11ac860a316eddbb4d73795d2c3b3e306f0cd
DESKTOP-TAFGOBQ\shev4 <s.v.shev4enk0@gmail.com>
2017-11-18 13:19:40 +0200

    7Levels transform.Translate

---
+++ Assets/InversTransform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InversTransform : MonoBehaviour
+{
+
+   
+    private Vector3 relative;
+
+    void Start()
+    {
+        relative = transform.position;
+        Debug.Log(relative);
+        relative = transform.InverseTransformDirection(0, 0, 1);
+        Debug.Log(relative);
+    }
+}
+
